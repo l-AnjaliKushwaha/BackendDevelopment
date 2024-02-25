a5b12b528077ebabae11dcd45bf9c5c3a92c3eaa
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-02-25 15:12:57 +0530

    Models

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express();
 
 const port = 4000;
 
+const profileData = {
+            name: "Anjali Kushwaha",
+            email: "anjali@gmail.com",
+            city: "Bhopal",
+            gender: "Female",
+            education: "B.Tech"
+}
+
 app.get('/', (req, res) => {
             res.send("Hello World!");
 });
@@ -13,6 +21,9 @@ app.get('/twitter', (req, res) => {
 });
 app.get('/login', (req, res) => {
             res.send("<h1>Please login to the page </h1>");
+})
+app.get('/data', (req, res) => {
+            res.json(profileData);
 })
 app.listen(process.env.PORT, () => {
             console.log(`Example app listening on port ${port}`);
